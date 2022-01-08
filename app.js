939ef138aa80023d9d825b28a6ace80d149c72d6
barbarabanach <krzewicka.barbara@gmail.com>
2022-01-08 14:03:10 +0100

    passing data from webpage to server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.use(bodyParser.urlencoded({extended: true}));
 //adding ejs
@@ -12,45 +13,24 @@ const port = 3000;
 app.get('/', (req, res) => {
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    // if(currentDay === 6 || currentDay == 0) {
-    //     day = "Weekend";
-    // } else {
-    //     day = "Weekday";
-    // }
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wendsday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-        default:
-            console.log(`Sorry, we are out of ${currentDay}.`);
-    }
+    var day = today.toLocaleDateString("en-US", options);
 
-
-    res.render("list", { kindOfDay: day });
+    res.render("list", { kindOfDay: day, newListItem: items });
 })
 
+app.post('/', (req, res) =>  {
+
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
 
 
 app.listen(port, () => {
